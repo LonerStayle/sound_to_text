8cd56ab460249f5361b92e481c15a84ccfb0b37a
Loner <LonerStayle@github.com>
2025-09-03 15:57:35 +0900

    add

--- Test.py
+++ Test.py
@@ -1,22 +1,15 @@
 from faster_whisper import WhisperModel
 from pathlib import Path
 
-AUDIO_PATH = "/Users/seobi/Documents/2025년8월8일3시수업.m4a"      # 여기에 m4a 경로
-OUT_BASENAME = Path(AUDIO_PATH).with_suffix("")  # 확장자 제거한 경로
+AUDIO_PATHS = [
+    "C:\\Users\\user\\Music\\음성 250903_090458.m4a",
+    "C:\\Users\\user\\Music\\음성 250903_120222.m4a",
+]
+
 
 # 모델 크기: tiny / base / small / medium / large-v3
 # 한국어면 small~medium 이상 권장, 정확도는 large-v3가 최고
-model = WhisperModel("large-v3", "cpu", compute_type="int8")  
-
-segments, info = model.transcribe(
-    AUDIO_PATH,
-    beam_size=5,
-    vad_filter=True,                   
-    vad_parameters=dict(min_silence_duration_ms=500),
-    language="ko"                      
-)
-
-print(f"Detected language: {info.language} (prob={info.language_probability:.2f})")
+model = WhisperModel("large-v3", "cuda", compute_type="int8")  
 
 # 자막/텍스트 저장
 def sec_to_srt_ts(s):
@@ -27,22 +20,33 @@ def sec_to_srt_ts(s):
     return f"{h:02d}:{m:02d}:{s:02d},{ms:03d}"
 
 def start():
-    srt_lines = []
-    txt_lines = []
-    step = 0 
-    for i, seg in enumerate(segments, start=1):
-        start = sec_to_srt_ts(seg.start)
-        end = sec_to_srt_ts(seg.end)
-        text = seg.text.strip()
-        srt_lines.append(f"{i}\n{start} --> {end}\n{text}\n")
-        txt_lines.append(text)    
-        step += 1
-        print(step)
+    for path in AUDIO_PATHS:
+        segments, info = model.transcribe(
+        path,
+        beam_size=5,
+        vad_filter=True,                   
+        vad_parameters=dict(min_silence_duration_ms=500),
+        language="ko"                      
+        )
+        print(f"Detected language: {info.language} (prob={info.language_probability:.2f})")
+        srt_lines = []
+        txt_lines = []
+        step = 0 
+        for i, seg in enumerate(segments, start=1):
+            start = sec_to_srt_ts(seg.start)
+            end = sec_to_srt_ts(seg.end)
+            text = seg.text.strip()
+            srt_lines.append(f"{i}\n{start} --> {end}\n{text}\n")
+            txt_lines.append(text)    
+            step += 1
+            print(step)
+        OUT_BASENAME = Path(path).with_suffix("")  # 확장자 제거한 경로
+        # 파일로 저장
+        print("파일저장 시작")
+        (OUT_BASENAME.with_suffix(".srt")).write_text("\n".join(srt_lines), encoding="utf-8")
+        (OUT_BASENAME.with_suffix(".txt")).write_text("\n".join(txt_lines), encoding="utf-8")
+        print("완료")
         
-    # 파일로 저장
-    print("파일저장 시작")
-    (OUT_BASENAME.with_suffix(".srt")).write_text("\n".join(srt_lines), encoding="utf-8")
-    (OUT_BASENAME.with_suffix(".txt")).write_text("\n".join(txt_lines), encoding="utf-8")
-    print("완료")
+
 
 
